3e7e99a0be3e815d8bb42d52dfe01a24bf8628f2
Sergey Yaroshuk <yaroshukmail@gmail.com>
2026-06-16 21:15:50 +0200

    fix(server): add statement-breakpoints to key migration (PGlite multi-statement parse)

--- packages/server/src/db/migrations/0001_familiar_mystique.sql
+++ packages/server/src/db/migrations/0001_familiar_mystique.sql
@@ -1,3 +1,3 @@
-ALTER TABLE "work_items" ADD COLUMN "key" text;
-UPDATE "work_items" SET "key" = "agent_id" WHERE "key" IS NULL;
+ALTER TABLE "work_items" ADD COLUMN "key" text;--> statement-breakpoint
+UPDATE "work_items" SET "key" = "agent_id" WHERE "key" IS NULL;--> statement-breakpoint
 ALTER TABLE "work_items" ALTER COLUMN "key" SET NOT NULL;
